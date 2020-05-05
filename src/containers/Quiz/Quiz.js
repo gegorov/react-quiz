015d3d0926cc594a874cabfcb22aab923ec9952e
gegorov <gegorov@yahoo.com>
2020-05-05 22:51:14 +0300

    Add sample quiz state

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,7 +5,57 @@ import classes from './Quiz.module.css';
 
 class Quiz extends Component {
   state = {
-    quiz: [],
+    activeQuestion: 0,
+    answerState: null, // { [id]: 'success' | 'error'}
+    quiz: [
+      {
+        id: 1,
+        question: 'What is the color of the sky?',
+        correctAnswerId: 2,
+        answers: [
+          {
+            id: 1,
+            text: 'Black',
+          },
+          {
+            id: 2,
+            text: 'Blue',
+          },
+          {
+            id: 3,
+            text: 'Red',
+          },
+          {
+            id: 4,
+            text: 'Green',
+          },
+        ],
+      },
+      {
+        id: 2,
+        question: 'When was Philadelphia founded?',
+        correctAnswerId: 3,
+        answers: [
+          {
+            id: 1,
+            text: '1704',
+          },
+          {
+            id: 2,
+            text: '1650',
+          },
+          {
+            id: 3,
+            text: '1682',
+          },
+          {
+            id: 4,
+            text: '1830',
+          },
+        ],
+      },
+
+    ],
   };
 
   render() {
