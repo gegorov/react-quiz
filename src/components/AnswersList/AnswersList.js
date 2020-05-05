36bdfb36d356d3206c677460d7f6fbbc488db924
gegorov <gegorov@yahoo.com>
2020-05-05 21:24:27 +0300

    Add AnswerList

---
+++ src/components/AnswersList/AnswersList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import AnswerItem from '../AnswerItem/AnswerItem';
+import classes from './AnswersList.module.css';
+
+const AnswersList = props => {
+  return (
+    <ul className={classes.AnswersList}>
+      { props.answers.map(
+          (answer, index) => <AnswerItem answer={answer} key={index}/>
+        )
+      }
+    </ul>
+  );
+}
+
+export default AnswersList;
