4304d2af462b88d89be143564fab881a8e35a7e7
gegorov <gegorov@yahoo.com>
2020-05-12 01:26:40 +0300

    Add basic Auth page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 
+import Button from '../../components/UI/Button/Button';
+
 import classes from './Auth.module.css';
 
 class Auth extends Component {
   state = {  }
+
+  loginHandler() {
+
+  }
+
+  registerHandler() {
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+  }
+
   render() { 
     return (
-      <div>
-        <h1>Auth works</h1>
+      <div className={classes.Auth}>
+        <div className={classes.AuthWrapper}>
+          <h1>Authentication</h1>
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <input/>
+            <input/>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >
+              Log in
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+            >
+              Register
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
