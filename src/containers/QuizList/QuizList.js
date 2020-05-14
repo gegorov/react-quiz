645292436c872fd732dd476c0bcc3961e7dfa1a1
gegorov <gegorov@yahoo.com>
2020-05-15 01:23:41 +0300

    Refactor QuizList to fetch data from api

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+
+import Loader from '../../components/UI/Loader/Loader';
 
 import classes from './QuizList.module.css';
 
 class QuizList extends Component {
-  state = {  }
-
-  renderQuizzes() {
-    return [1, 2, 3].map((quiz, index) => {
-      return(
-        <li key={index}>
-          <NavLink to={`/quiz/${quiz}`}> Test {quiz}</NavLink>
-        </li>
-      )
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      quizzes: [],
+      loading: true,
+    };
   }
 
-  render() { 
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get('https://quiz-a3324.firebaseio.com/quizzes.json');
+      const quizzes = [];
+      Object.keys(response.data).forEach((key, index) => {
+        quizzes.push({
+          id: key,
+          quiz: response.data[key],
+          name: `Quiz #${index + 1}`,
+        });
+      });
+
+      this.setState({
+        quizzes: [...quizzes],
+        loading: false,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  renderQuizzes() {
+    const { quizzes } = this.state;
+    return quizzes.map((quiz) => (
+      <li key={quiz.id}>
+        <NavLink to={`/quiz/${quiz.id}`}>
+          {' '}
+          {quiz.name}
+        </NavLink>
+      </li>
+    ));
+  }
+
+  render() {
+    const { loading } = this.state;
     return (
       <div className={classes.QuizList}>
         <h1>Quiz List</h1>
-        <ul>
-          { this.renderQuizzes() }
-        </ul>
+        {
+          loading
+            ? <Loader />
+            : (
+              <ul>
+                { this.renderQuizzes() }
+              </ul>
+            )
+        }
+
       </div>
     );
   }
