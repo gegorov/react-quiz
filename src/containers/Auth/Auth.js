96d320308930133b15ed8d935bba0c260fa92a5a
gegorov <gegorov@yahoo.com>
2020-05-15 02:18:07 +0300

    Fix linter errors in Auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,74 +7,70 @@ import Input from '../../components/UI/Input/Input';
 import classes from './Auth.module.css';
 
 class Auth extends Component {
-  state = {
-    isFormValid: false,
-    formControls: {
-      email: {
-        value: '',
-        type: 'email',
-        label: 'Email',
-        errorMessage: 'Please enter correct email',
-        valid: false,
-        touched: false,
-        validation: {
-          required: true,
-          email: true,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFormValid: false,
+      formControls: {
+        email: {
+          value: '',
+          type: 'email',
+          label: 'Email',
+          errorMessage: 'Please enter correct email',
+          valid: false,
+          touched: false,
+          validation: {
+            required: true,
+            email: true,
+          },
+        },
+        password: {
+          value: '',
+          type: 'password',
+          label: 'Password',
+          errorMessage: 'Please enter correct password',
+          valid: false,
+          touched: false,
+          validation: {
+            required: true,
+            minLength: 6,
+          },
+        },
       },
-      password: {
-        value: '',
-        type: 'password',
-        label: 'Password',
-        errorMessage: 'Please enter correct password',
-        valid: false,
-        touched: false,
-        validation: {
-          required: true,
-          minLength: 6,
-        }
-      },
-    }
+    };
   }
 
-  loginHandler() {
 
-  }
-
-  registerHandler() {
-
-  }
-
-  submitHandler = event => {
+  submitHandler = (event) => {
     event.preventDefault();
   }
 
   onChangeHandler = (event, controlName) => {
-    const formControls = {
-      ...this.state.formControls,
-    };
+    const { formControls } = this.state;
 
-    const control = { ...formControls[controlName] };
+    const formControlsCopy = { ...formControls };
+
+    const control = { ...formControlsCopy[controlName] };
 
     control.value = event.target.value;
     control.touched = true;
     control.valid = this.validateControl(control.value, control.validation);
 
-    formControls[controlName] = control;
+    formControlsCopy[controlName] = control;
 
     let isFormValid = true;
 
-    Object.keys(formControls).forEach(name => {
-      isFormValid = formControls[name].valid && isFormValid;
-    })
+    Object.keys(formControlsCopy).forEach((name) => {
+      isFormValid = formControlsCopy[name].valid && isFormValid;
+    });
 
     this.setState({
-      formControls,
+      formControls: formControlsCopy,
       isFormValid,
-    })
+    });
   }
 
-  validateControl(value, validation) {
+  validateControl = (value, validation) => {
     if (!validation) {
       return true;
     }
@@ -96,25 +92,36 @@ class Auth extends Component {
     return isValid;
   }
 
+
+  loginHandler() {
+
+  }
+
+  registerHandler() {
+
+  }
+
   renderInputs() {
-    return Object.keys(this.state.formControls).map((controlName, index) => {
-      const control = this.state.formControls[controlName];
+    const { formControls } = this.state;
+    return Object.keys(formControls).map((controlName, index) => {
+      const control = formControls[controlName];
       return (
         <Input
-          key={controlName + index}
+          key={controlName + String(index)}
           type={control.type}
           value={control.value}
           valid={control.valid}
           touched={control.touched}
           shouldValidate={!!control.validation}
           errorMessage={control.errorMessage}
-          onChange={event => this.onChangeHandler(event, controlName)}
+          onChange={(event) => this.onChangeHandler(event, controlName)}
         />
-      )
-    })
+      );
+    });
   }
 
-  render() { 
+  render() {
+    const { isFormValid } = this.state;
     return (
       <div className={classes.Auth}>
         <div className={classes.AuthWrapper}>
@@ -126,14 +133,14 @@ class Auth extends Component {
             <Button
               type="success"
               onClick={this.loginHandler}
-              disabled={!this.state.isFormValid}
+              disabled={!isFormValid}
             >
               Log in
             </Button>
             <Button
               type="primary"
               onClick={this.registerHandler}
-              disabled={!this.state.isFormValid}
+              disabled={!isFormValid}
             >
               Register
             </Button>
