d4e6e34456df35fe89d0023ada97ecf51c8d9e00
gegorov <gegorov@yahoo.com>
2020-05-15 21:06:20 +0300

    Add redux to the project

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store from './store/index';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 const app = (
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
 ReactDOM.render(
   app,
