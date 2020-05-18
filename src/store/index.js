13bbe13b1db302431d661db6afee817e7f0dfd15
gegorov <gegorov@yahoo.com>
2020-05-18 22:55:50 +0300

    Enable action tracing in redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,9 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
 const middleware = [thunk];
-const store = createStore(rootReducer, composeWithDevTools(
+const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 10 });
+
+const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(...middleware),
   // other store enhancers if any
 ));
